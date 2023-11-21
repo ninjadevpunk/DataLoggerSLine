31c6bec4fa533ef89472f748d93b4646952451ba
Anda Mbambo <andambambo@gmail.com>
2023-11-21 18:26:38 +0200

    Added test code for forgot password page navigation and sign up window.

---
+++ Views/loginPage01.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Data_Logger_1._3.Views
+{
+    /// <summary>
+    /// Interaction logic for loginPage01.xaml
+    /// </summary>
+    public partial class loginPage01 : Page
+    {
+        public loginPage01()
+        {
+            InitializeComponent();
+        }
+
+        private void on_FORGOTPASSWORD_clicked(object sender, MouseButtonEventArgs e)
+        {
+            var loginWindow = Window.GetWindow(this) as Login;
+
+            // Navigate to Forgot Password Page
+            loginWindow?.Navigator("Views/loginPage02.xaml");
+        }
+
+        private void on_SIGNUP_clicked(object sender, MouseButtonEventArgs e)
+        {
+
+            try
+            {
+
+                // Open SignUp window
+                SignUp signUpWindow = new SignUp();
+                signUpWindow.Show();
+
+                var loginWindow = Window.GetWindow(this) as Login;
+
+                // Close the current window
+                loginWindow.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
