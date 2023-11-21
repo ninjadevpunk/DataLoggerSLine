6aa548de3a73460592e10cafc80a019fb9915a6a
Anda Mbambo <andambambo@gmail.com>
2023-11-21 18:21:02 +0200

    Added test code for login.xaml.CS so I could see whether the navigation buttons would work as well as the close and minimise button. Also added code for window start up location. Remember to modify to MVVM structure later.

--- Views/Login.xaml.cs
+++ Views/Login.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media;
 
 namespace Data_Logger_1._3.Views
 {
@@ -7,9 +8,18 @@ namespace Data_Logger_1._3.Views
     /// </summary>
     public partial class Login : Window
     {
+        private readonly Brush? enabledColor = TryParseBrush("iconCOLOURAccent02");
+        private readonly Brush? disabledColor = TryParseBrush("AccentColour");
+
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        public void Navigator(string pageUri)
+        {
+            frame_LOGIN.NavigationService.Navigate(new Uri(pageUri, UriKind.Relative));
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -36,6 +46,46 @@ namespace Data_Logger_1._3.Views
         {
             // Close the entire application
             Application.Current.Shutdown();
+        }
+
+        private void on_Navigation(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            UpdateNavigationButtons();
+        }
+
+        private void UpdateNavigationButtons()
+        {
+            button_BACK.IsEnabled = frame_LOGIN.NavigationService.CanGoBack;            
+            button_FORWARD.IsEnabled = frame_LOGIN.NavigationService.CanGoForward;
+
+            icon_BACK.Fill = button_BACK.IsEnabled ? enabledColor! : disabledColor!;
+            icon_FORWARD.Fill = button_FORWARD.IsEnabled ? enabledColor! : disabledColor!;
+        }
+
+        private void on_BACK_clicked(object sender, RoutedEventArgs e)
+        {
+            if (frame_LOGIN.NavigationService?.CanGoBack ?? false)
+                frame_LOGIN.NavigationService?.GoBack();
+        }
+
+        private void on_FORWARD_clicked(object sender, RoutedEventArgs e)
+        {
+            if (frame_LOGIN.NavigationService?.CanGoForward ?? false)
+                frame_LOGIN.NavigationService?.GoForward();
+        }
+
+        private static Brush? TryParseBrush(string value)
+        {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+            try
+            {
+                return (Brush)Application.Current.FindResource(value);
+            }
+            catch (ResourceReferenceKeyNotFoundException)
+            {
+                return Brushes.Transparent;
+            }
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
         }
     }
 }
