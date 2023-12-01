a5d32579a878777577ccb4b00dbc57647cb3ed8b
Anda Mbambo <andambambo@gmail.com>
2023-12-01 20:28:05 +0200

    Created this control from the ground up with a similar structure to the input field in the DLSPrototype02 project. Added bound fields for external TextBox and TextBlock styling.
    Removed namespaces. Added properties for the control code behind. Created dependency properties, memeber functions, event handlers and routed events.

--- Components/PlainInputField.xaml.cs
+++ Components/PlainInputField.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Data_Logger_1._3.Components
 {
@@ -20,9 +8,174 @@ namespace Data_Logger_1._3.Components
     /// </summary>
     public partial class PlainInputField : UserControl
     {
+        public bool Status { get; set; } = true;
+        public string Temp { get; set; } = "";
+
         public PlainInputField()
         {
             InitializeComponent();
         }
+
+        #region Dependency Properties
+
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty PlaceholderTextProperty =
+            DependencyProperty.Register("PlaceholderText", typeof(string), typeof(PlainInputField));
+
+
+        public string UserInput
+        {
+            get { return (string)GetValue(UserInputProperty); }
+            set { SetValue(UserInputProperty, value); }
+        }
+
+        public static readonly DependencyProperty UserInputProperty =
+            DependencyProperty.Register("UserInput", typeof(string), typeof(PlainInputField));
+
+
+
+        // Text Alignment
+        public string Aligned
+        {
+            get { return (string)GetValue(AlignedProperty); }
+            set { SetValue(AlignedProperty, value); }
+        }
+
+        public static readonly DependencyProperty AlignedProperty =
+            DependencyProperty.Register("Aligned", typeof(string), typeof(PlainInputField));
+
+
+        // TextBox Style
+        public Style UserTextBoxStyle
+        {
+            get { return (Style)GetValue(UserTextBoxStyleProperty); }
+            set { SetValue(UserTextBoxStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty UserTextBoxStyleProperty =
+            DependencyProperty.Register("UserTextBoxStyle", typeof(Style), typeof(PlainInputField));
+
+        // TextBlock Style
+        public Style UserTextBlockStyle
+        {
+            get { return (Style)GetValue(UserTextBlockStyleProperty); }
+            set { SetValue(UserTextBlockStyleProperty, value); }
+        }
+
+        public static readonly DependencyProperty UserTextBlockStyleProperty =
+            DependencyProperty.Register("UserTextBlockStyle", typeof(Style), typeof(PlainInputField));
+
+        #endregion
+
+
+
+
+        #region Member Functions
+
+
+        private void on_inputText_lostFocus(object sender, RoutedEventArgs e)
+        {
+            showPlaceholderText();
+        }
+
+
+        public void showPlaceholderText()
+        {
+            if (this.inputText_text.Text == null)
+            {
+                return;
+            }
+            else if (this.inputText_text.Text == "" && this.text_PlaceholderText.Text == "")
+            {
+                this.text_PlaceholderText.Text = Temp;
+                Status = true;
+            }
+        }
+
+
+        /* This function assumes there is Placeholder Text if assert is true */
+        public void showPlaceholderText(bool show, bool assert)
+        {
+            if (show)
+            {
+                this.text_PlaceholderText.Text = Temp;
+                Status = true;
+            }
+            else if (show && assert || this.inputText_text.Text == "")
+            {
+                this.text_PlaceholderText.Text = PlaceholderText;
+                Status = true;
+            }
+        }
+
+
+
+        #endregion
+
+
+
+
+
+
+
+
+        #region Event Handlers
+
+
+        private void on_inputText_text_changed(object sender, TextChangedEventArgs e)
+        {
+            if (Status)
+            {
+                Temp = this.text_PlaceholderText.Text;
+                this.text_PlaceholderText.Text = "";
+                Status = false;
+            }
+
+            var ev = new RoutedEventArgs(TextChangedEvent);
+            RaiseEvent(ev);
+        }
+
+        
+
+
+
+
+
+
+        #endregion
+
+
+
+
+        #region Routed Events
+
+
+        public event RoutedEventHandler TextChanged
+        {
+            add { AddHandler(TextChangedEvent, value); }
+            remove { RemoveHandler(TextChangedEvent, value); }
+        }
+
+        public static readonly RoutedEvent TextChangedEvent = EventManager.RegisterRoutedEvent(
+            "TextChanged",
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(PlainInputField));
+
+
+
+
+
+
+
+
+        #endregion
+
     }
 }
