766f988455c0921feb272625016e6c60c2d73fef
Anda Mbambo <andambambo@gmail.com>
2023-12-01 20:21:16 +0200

    Created a vastly improved version of the previous DateTime control. Much simpler and neater. Styles added to relevant resource files - DatePickers.

---
+++ Components/DateInputField.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Data_Logger_1._3.Components
+{
+    /// <summary>
+    /// Interaction logic for DateInputField.xaml
+    /// </summary>
+    public partial class DateInputField : UserControl
+    {
+        public DateInputField()
+        {
+            InitializeComponent();
+            this.datePicker_DATE.SelectedDateFormat = DatePickerFormat.Long;
+            
+        }
+
+        #region Dependency Properties
+
+
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(TheDateProperty); }
+            set { SetValue(TheDateProperty, value); }
+        }
+
+        public static readonly DependencyProperty TheDateProperty =
+            DependencyProperty.Register("TheDate", typeof(DateTime), typeof(DateInputField),
+                
+                new PropertyMetadata(DateTime.Now)
+                );
+
+
+
+        //public string PlaceholderText
+        //{
+        //    get { return (string)GetValue(PlaceholderTextProperty); }
+        //    set { SetValue(PlaceholderTextProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for PlaceholderText.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty PlaceholderTextProperty =
+        //    DependencyProperty.Register("PlaceholderText", typeof(string), typeof(DateInputField));
+
+
+
+
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+        #region Member Functions
+
+
+        public void setDate()
+        {
+            this.datePicker_DATE.SelectedDate = DateTime.Now;
+        }
+
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+        #region Event Handlers 
+        private void on_DATE_changed(object sender, SelectionChangedEventArgs e)
+        {
+            var ev = new RoutedEventArgs(DateChangedEvent);
+            RaiseEvent(ev);
+        }
+
+
+        #endregion
+
+
+
+
+
+
+        #region Routed Events
+
+
+        public event RoutedEventHandler DateChanged
+        {
+            add { AddHandler(DateChangedEvent, value); }
+            remove { RemoveHandler(DateChangedEvent, value); }
+        }
+
+        public static readonly RoutedEvent DateChangedEvent = EventManager.RegisterRoutedEvent(
+            "DateChanged",
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(DateInputField));
+
+
+
+
+        #endregion
+
+
+
+
+
+
+
+
+    }
+}
