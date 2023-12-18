ab1c52e73c2e24beafbcc6e64e4c16953e07abc4
Anda Mbambo <andambambo@gmail.com>
2023-12-18 20:34:26 +0200

    Created graphics fields.

---
+++ Components/Subcontrols/graphics_UserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Data_Logger_1._3.Components.Subcontrols
+{
+    /// <summary>
+    /// Interaction logic for graphics_UserControl.xaml
+    /// </summary>
+    public partial class graphics_UserControl : UserControl
+    {
+        public graphics_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
