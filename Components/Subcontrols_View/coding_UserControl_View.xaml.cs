c55d0d0bfc98f45ace66bd49a60dc54fb8766ceb
Anda Mbambo <andambambo@gmail.com>
2023-12-20 23:42:36 +0200

    Created the View version of the Subcontrols.

---
+++ Components/Subcontrols_View/coding_UserControl_View.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Data_Logger_1._3.Components.Subcontrols_View
+{
+    /// <summary>
+    /// Interaction logic for coding_UserControl_View.xaml
+    /// </summary>
+    public partial class coding_UserControl_View : UserControl
+    {
+        public coding_UserControl_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
