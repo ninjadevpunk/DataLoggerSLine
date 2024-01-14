e8f912acd09f3d5dc8a1a2cf63982d4a5edebe33
Anda Mbambo <andambambo@gmail.com>
2024-01-14 22:05:13 +0200

    Created Checklist model.

---
+++ Models/CheckList.cs
@@ -0,0 +1,33 @@
+﻿namespace Data_Logger_1._3.Models
+{
+    public class CheckList : List<CheckListItem>
+    {
+
+        /* CONSTRUCTORS */
+
+
+        public CheckList() { }
+
+        public CheckList(bool isChecked, string item)
+        {
+            this.Add(new CheckListItem(isChecked, item));
+        }
+
+        public CheckList(CheckListItem item)
+        {
+            this.Add(item);
+        }
+
+
+        
+        /* MEMBER FUNCTIONS */
+
+
+
+        public void Add(bool isChecked, string item)
+        {
+            this.Add(new CheckListItem(isChecked, item));
+        }
+
+    }
+}
