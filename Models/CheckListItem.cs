819a10e53128870566ad6d2be80de3fbcf7b304c
Anda Mbambo <andambambo@gmail.com>
2024-01-14 22:07:28 +0200

    Created CheckListItem model.

---
+++ Models/CheckListItem.cs
@@ -0,0 +1,55 @@
+﻿namespace Data_Logger_1._3.Models
+{
+    public class CheckListItem
+    {
+        /* PROPERTIES */
+
+
+        public bool IsChecked { get; set; } = false;
+
+        public string Item { get; set; } = "";
+
+
+
+
+        /* CONSTRUCTORS */
+
+
+
+        public CheckListItem() { }
+
+        public CheckListItem(bool isChecked, string item)
+        {
+            IsChecked = isChecked;
+            Item = item;
+        }
+
+
+
+        /* OVERLOADS */
+
+
+
+        public override bool Equals(object? obj)
+        {
+            return obj is CheckListItem item &&
+                   IsChecked == item.IsChecked &&
+                   Item == item.Item;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(IsChecked, Item);
+        }
+
+        public static bool operator ==(CheckListItem left, CheckListItem right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CheckListItem left, CheckListItem right)
+        {
+            return !left.Equals(right);
+        }
+    }
+}
