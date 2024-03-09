3232792cf46ac4c2f887c53785f5585eec6df41e
Anda Mbambo <andambambo@gmail.com>
2024-03-09 17:56:09 +0200

    Created a Message for removal of items in the log cache lists.

---
+++ Messages/RemoveItemMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace Data_Logger_1._3.Messages
+{
+    public class RemoveItemMessage
+    {
+        public object Target { get; }
+
+        public RemoveItemMessage(object itemToRemove)
+        {
+            Target = itemToRemove;
+        }
+    }
+}
