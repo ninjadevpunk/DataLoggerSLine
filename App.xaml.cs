0fd726cb57fe779023edbdb4a63a187f8c38c092
Anda Mbambo <andambambo@gmail.com>
2024-03-09 22:17:00 +0200

    Removed the StartupUri that caused the MainWindow and Login view to clash on opening.
    Added dependency injection to the App.xaml.cs.
    
    Added basic start up functionality to enable testing of the app.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Data_Logger_1._3.Services;
+using Data_Logger_1._3.Views;
+using Firebase.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
 
 namespace Data_Logger_1._3
 {
@@ -7,6 +13,41 @@ namespace Data_Logger_1._3
     /// </summary>
     public partial class App : Application
     {
+
+        private readonly IHost _host;
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+                    var firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY");
+                    var firebaseDomain = context.Configuration.GetValue<string>("FIREBASE_DOMAIN");
+
+                    service.AddTransient<SignUp>();
+                    service.AddTransient<Splashscreen>();
+                    service.AddTransient<Login>();
+                    service.AddSingleton<MainWindow>();
+                    service.AddSingleton(new AuthService(firebaseApiKey, firebaseDomain));
+                    service.AddSingleton<NavigationService>();
+                })
+                .Build();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            var navigationService = _host.Services.GetRequiredService<NavigationService>();
+            navigationService.SetHost(_host);
+            navigationService.NavigateToLogin();
+
+
+
+
+
+
+
+            base.OnStartup(e);
+        }
     }
 
 }
