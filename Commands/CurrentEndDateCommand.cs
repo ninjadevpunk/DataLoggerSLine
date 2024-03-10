ae7e602037f5c01c770b9631ff229bbd4f8ec3df
Anda Mbambo <andambambo@gmail.com>
2024-03-10 13:40:03 +0200

    Created and implemented the current date commands.
    Added AddPostItCommand for the sticky notes button.

---
+++ Commands/CurrentEndDateCommand.cs
@@ -0,0 +1,35 @@
+﻿using Data_Logger_1._3.ViewModels.Dialogs;
+using MVVMEssentials.Commands;
+
+namespace Data_Logger_1._3.Commands
+{
+    public class CurrentEndDateCommand : CommandBase
+    {
+        private readonly LoggerCreateViewModel _viewModel;
+
+        public CurrentEndDateCommand(LoggerCreateViewModel viewModel)
+        {
+
+
+            try
+            {
+                _viewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+
+        }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.EndDate = DateTime.Now;
+            _viewModel.EndHours = DateTime.Now.Hour;
+            _viewModel.EndMinutes = DateTime.Now.Minute;
+            _viewModel.EndSeconds = DateTime.Now.Second;
+            _viewModel.EndMilliseconds = DateTime.Now.Millisecond;
+        }
+    }
+}
---
+++ Commands/PostIt/AddPostItCommand.cs
@@ -0,0 +1,34 @@
+﻿using Data_Logger_1._3.Services;
+using Data_Logger_1._3.ViewModels.Dialogs;
+using MVVMEssentials.Commands;
+
+namespace Data_Logger_1._3.Commands.PostIt
+{
+    public class AddPostItCommand : CommandBase
+    {
+        private readonly NavigationService _navigationService;
+        private readonly LoggerCreateViewModel _loggerCreateViewModel;
+
+        public AddPostItCommand(NavigationService navigationService, LoggerCreateViewModel loggerCreateViewModel)
+        {
+
+
+            try
+            {
+                _navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+                _loggerCreateViewModel = loggerCreateViewModel ?? throw new ArgumentNullException(nameof(loggerCreateViewModel));
+            }
+            catch (Exception)
+            {
+                //
+            }
+
+
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationService.NavigateToPostItCreator(_loggerCreateViewModel);
+        }
+    }
+}
