9648de63e2aa1501348a90394f5e50692a61f76f
Anda Mbambo <andambambo@gmail.com>
2024-03-15 18:22:42 +0200

    Added smooth progressbar animation for splashscreen.
    Added datacontexts to dependencies.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Data_Logger_1._3.Services;
+using Data_Logger_1._3.ViewModels;
 using Data_Logger_1._3.Views;
-using Firebase.Auth;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace Data_Logger_1._3
 {
@@ -15,6 +17,7 @@ namespace Data_Logger_1._3
     {
 
         private readonly IHost _host;
+        private static readonly Random random = new();
         public App()
         {
             _host = Host.CreateDefaultBuilder()
@@ -22,23 +25,48 @@ namespace Data_Logger_1._3
                 {
                     var firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY");
                     var firebaseDomain = context.Configuration.GetValue<string>("FIREBASE_DOMAIN");
-
-                    service.AddTransient<SignUp>();
-                    service.AddTransient<Splashscreen>();
-                    service.AddTransient<Login>();
-                    service.AddSingleton<MainWindow>();
                     service.AddSingleton(new AuthService(firebaseApiKey, firebaseDomain));
-                    service.AddSingleton<NavigationService>();
+                    service.AddSingleton((services) => new NavigationService(services.GetRequiredService<AuthService>()));
+                    service.AddTransient<Splashscreen>();
+                    service.AddSingleton<Login>();
+                    service.AddTransient((services) => new loginPage01(services.GetRequiredService<NavigationService>())
+                    {
+                        DataContext = new LoginViewModel(services.GetRequiredService<AuthService>(), services.GetRequiredService<NavigationService>())
+                    }
+                    );
+                    service.AddTransient((services) => new loginPage02(services.GetRequiredService<AuthService>(), services.GetRequiredService<NavigationService>())
+                    {
+                        DataContext = new ForgotPasswordViewModel(services.GetRequiredService<AuthService>(), services.GetRequiredService<NavigationService>())
+                    }
+                    );
+                    service.AddSingleton((services) => new SignUp(services.GetRequiredService<NavigationService>())
+                    {
+                        DataContext = new SignUpViewModel(services.GetRequiredService<AuthService>(), services.GetRequiredService<NavigationService>())
+                    }
+                    );
+                    service.AddSingleton<MainWindow>();
                 })
                 .Build();
         }
 
-        protected override void OnStartup(StartupEventArgs e)
+        protected override async void OnStartup(StartupEventArgs e)
         {
+            var splash = _host.Services.GetRequiredService<Splashscreen>();
+            splash.Show();
+
+
+            await AnimateProgressBar(splash.progressBar_splashscreen, 33);
+
 
             var navigationService = _host.Services.GetRequiredService<NavigationService>();
+
             navigationService.SetHost(_host);
+            await AnimateProgressBar(splash.progressBar_splashscreen, 66);
+            await AnimateProgressBar(splash.progressBar_splashscreen, 100);
+
             navigationService.NavigateToLogin();
+
+            splash.Close();
 
 
 
@@ -47,6 +75,19 @@ namespace Data_Logger_1._3
 
 
             base.OnStartup(e);
+        }
+
+        private async Task AnimateProgressBar(ProgressBar progressBar, double targetValue)
+        {
+            DoubleAnimation animation = new DoubleAnimation(targetValue, TimeSpan.FromSeconds(1));
+
+            var tcs = new TaskCompletionSource<bool>();
+            animation.Completed += (s, e) => tcs.SetResult(true);
+
+            progressBar.BeginAnimation(ProgressBar.ValueProperty, animation);
+
+            await tcs.Task;
+            await Task.Delay(random.Next(350, 1300)); // Wait for a short duration after each animation
         }
     }
 
