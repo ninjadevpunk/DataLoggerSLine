50c143a0b7afdf7deaff65dc671a573cb96d07f8
Anda Mbambo <andambambo@gmail.com>
2024-05-18 14:55:51 +0200

    Created Editor.

---
+++ Views/Dialogs/LoggerEditPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Data_Logger_1._3.Views.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for LoggerEditPage.xaml
+    /// </summary>
+    public partial class LoggerEditPage : Page
+    {
+        public LoggerEditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
