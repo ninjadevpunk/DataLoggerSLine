a989d2a64caf22b58be7cb2fe1c3184546a5308b
Anda Mbambo <andambambo@gmail.com>
2024-06-08 01:02:08 +0200

    Cosmetic changes to make splashscreen look less obtrusive.

--- Views/Splashscreen.xaml.cs
+++ Views/Splashscreen.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace Data_Logger_1._3.Views
 {
@@ -13,7 +12,7 @@ namespace Data_Logger_1._3.Views
             InitializeComponent();
         }
 
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // Calculate the new left position to center the window in the imaginary 3rd column
             double fourthColumnWidth = SystemParameters.PrimaryScreenWidth / 4.0;
@@ -28,18 +27,6 @@ namespace Data_Logger_1._3.Views
             this.Left = newLeft;
             this.Top = newTop;
 
-            // Simulate some time-consuming task
-            //for (int i = 0; i <= 100; i++)
-            //{
-            //    // Update the progress bar
-            //    progressBar_splashscreen.Dispatcher.Invoke(() => progressBar_splashscreen.Value = i);
-
-            //    // Update the text block
-            //    text_progress.Dispatcher.Invoke(() => text_progress.Text = $"{i}%");
-
-            //    // Simulate work being done
-            //    await Task.Delay(50);
-            //}
         }
     }
 }
