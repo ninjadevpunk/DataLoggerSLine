6c034ee6fb87a0a52acb4bde4aac16cbf0a6e7b5
Anda Mbambo <andambambo@gmail.com>
2024-08-08 23:10:51 +0200

    Created and implemented the Eraser and Highlighter commands.

---
+++ Commands/PostItCommands/ToolCommands/EraserCommand.cs
@@ -0,0 +1,61 @@
+﻿using Data_Logger_1._3.ViewModels.Dialogs;
+using MVVMEssentials.Commands;
+using static Data_Logger_1._3.ViewModels.Dialogs.CreatePostItViewModel;
+
+namespace Data_Logger_1._3.Commands.PostItCommands.ToolCommands
+{
+    public class EraserCommand : CommandBase
+    {
+        private readonly CreatePostItViewModel _createPostItViewModel;
+
+        public EraserCommand(CreatePostItViewModel createPostItViewModel)
+        {
+            try
+            {
+                _createPostItViewModel = createPostItViewModel ?? throw new ArgumentNullException(nameof(createPostItViewModel));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Argument null exception: {ex.Message}.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred: {ex.Message}");
+            }
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                switch (_createPostItViewModel.FieldToEdit)
+                {
+                    case PostItField.Error:
+                        {
+                            _createPostItViewModel.Error = string.Empty;
+                            break;
+                        }
+                    case PostItField.Solution:
+                        {
+                            _createPostItViewModel.Solution = string.Empty;
+                            break;
+                        }
+                    case PostItField.Suggestion:
+                        {
+                            _createPostItViewModel.Suggestion = string.Empty;
+                            break;
+                        }
+                    case PostItField.Comment:
+                        {
+                            _createPostItViewModel.Comment = string.Empty;
+                            break;
+                        }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Commands/PostItCommands/ToolCommands/HighlighterCommand.cs
@@ -0,0 +1,81 @@
+﻿using Data_Logger_1._3.ViewModels.Dialogs;
+using MVVMEssentials.Commands;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using static Data_Logger_1._3.ViewModels.Dialogs.CreatePostItViewModel;
+
+namespace Data_Logger_1._3.Commands.PostItCommands.ToolCommands
+{
+    public class HighlighterCommand : CommandBase
+    {
+        private readonly CreatePostItViewModel _createPostItViewModel;
+
+        public HighlighterCommand(CreatePostItViewModel createPostItViewModel)
+        {
+            try
+            {
+                _createPostItViewModel = createPostItViewModel ?? throw new ArgumentNullException(nameof(createPostItViewModel));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Argument null exception: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred: {ex.Message}");
+            }
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                var editor = _createPostItViewModel.ActiveEditor;
+                if (editor != null)
+                {
+                    switch (_createPostItViewModel.FieldToEdit)
+                    {
+                        case PostItField.Error:
+                            {
+                                editor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Orange);
+                                break;
+                            }
+                        case PostItField.Solution:
+                            {
+                                editor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Lime);
+                                break;
+                            }
+                        case PostItField.Suggestion:
+                            {
+                                editor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Pink);
+                                break;
+                            }
+                        case PostItField.Comment:
+                            {
+                                editor.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, Brushes.Yellow);
+                                break;
+                            }
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+
+        private static Brush? TryParseBrush(string value)
+        {
+            try
+            {
+                return (Brush)Application.Current.FindResource(value);
+            }
+            catch (ResourceReferenceKeyNotFoundException)
+            {
+                return Brushes.Transparent;
+            }
+        }
+    }
+}
