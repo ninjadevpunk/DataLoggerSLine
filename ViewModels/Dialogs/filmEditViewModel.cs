04fd7cc9887df60491cee8ef98c6111aa47ccd70
Anda Mbambo <andambambo@gmail.com>
2024-08-12 00:48:23 +0200

    Implemented the edit view models for film and flexi logs.

--- ViewModels/Dialogs/filmEditViewModel.cs
+++ ViewModels/Dialogs/filmEditViewModel.cs
@@ -1,6 +1,20 @@
-﻿namespace Data_Logger_1._3.ViewModels.Dialogs
+﻿using Data_Logger_1._3.Commands.LoggerCommands;
+using Data_Logger_1._3.Services;
+using Data_Logger_1._3.ViewModels.Dashboard;
+using Data_Logger_1._3.ViewModels.LogViewModels;
+using MVVMEssentials.ViewModels;
+
+namespace Data_Logger_1._3.ViewModels.Dialogs
 {
-    public class filmEditViewModel
+    public class filmEditViewModel : filmCreateViewModel
     {
+        private readonly FilmLOGViewModel _filmLOGViewModel;
+        public filmEditViewModel(NavigationService navigationService, LogCacheViewModel logCacheViewModel, DataService dataService, ViewModelBase viewModelBase) : base(navigationService, logCacheViewModel, dataService)
+        {
+            _filmLOGViewModel = (FilmLOGViewModel)viewModelBase;
+
+            AnnotateCommand = null;
+            EditCommand = new AnnotateCommand(ActionType.Edit, Context, _navigationService, this, _logCacheViewModel, _dataService, _filmLOGViewModel);
+        }
     }
 }
--- ViewModels/Dialogs/flexiEditViewModel.cs
+++ ViewModels/Dialogs/flexiEditViewModel.cs
@@ -1,12 +1,21 @@
-﻿using Data_Logger_1._3.Services;
+﻿using Data_Logger_1._3.Commands.LoggerCommands;
+using Data_Logger_1._3.Services;
 using Data_Logger_1._3.ViewModels.Dashboard;
+using Data_Logger_1._3.ViewModels.LogViewModels;
+using MVVMEssentials.ViewModels;
 
 namespace Data_Logger_1._3.ViewModels.Dialogs
 {
     public class flexiEditViewModel : flexiCreateViewModel
     {
-        public flexiEditViewModel(NavigationService navigationService, LogCacheViewModel logCacheViewModel, DataService dataService) : base(navigationService, logCacheViewModel, dataService)
+        private readonly FlexiLOGViewModel _flexiLOGViewModel;
+
+        public flexiEditViewModel(NavigationService navigationService, LogCacheViewModel logCacheViewModel, DataService dataService, ViewModelBase viewModelBase) : base(navigationService, logCacheViewModel, dataService)
         {
+            _flexiLOGViewModel = (FlexiLOGViewModel)viewModelBase;
+
+            AnnotateCommand = null;
+            EditCommand = new AnnotateCommand(ActionType.Edit, Context, _navigationService, this, _logCacheViewModel, _dataService, _flexiLOGViewModel);
         }
     }
 }
