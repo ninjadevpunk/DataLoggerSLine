76ba1062d211eb4c6e7bd5d747f663baa725ddf1
Anda Mbambo <andambambo@gmail.com>
2024-08-26 01:58:16 +0200

    ResetLoggerCommand fully implemented for all log types.

--- Commands/ResetLoggerCommand.cs
+++ Commands/ResetLoggerCommand.cs
@@ -62,9 +62,7 @@ namespace Data_Logger_1._3.Commands
                             var codingViewModel = (codeCreateViewModel)_viewModel;
 
                             if (isQt)
-                                codingViewModel.ApplicationName = Qt;
-                            else if (isAndroidStudio)
-                                codingViewModel.ApplicationName = Android;
+                                codingViewModel.ApplicationName = Qt;                                
                             else if (isVisualStudio)
                                 codingViewModel.ApplicationName = VisualStudio;
 
@@ -72,6 +70,45 @@ namespace Data_Logger_1._3.Commands
                             codingViewModel.Type = "Build";
                             codingViewModel.BugsFound = 0;
                             codingViewModel.ApplicationOpened = false;
+
+                            if(isAndroidStudio)
+                            {
+                                codingViewModel.ApplicationName = Android;
+                                var AScodingViewModel = (AScodeCreateViewModel)codingViewModel;
+                                AScodingViewModel.IsSimple = false;
+
+                                AScodingViewModel.SyncHours = 0;
+                                AScodingViewModel.SyncMinutes = 0;
+                                AScodingViewModel.SyncSeconds = 0;
+                                AScodingViewModel.SyncMilliseconds = 0;
+
+                                AScodingViewModel.IsConsidered = false;
+                                AScodingViewModel.GradleDaemonHours = 0;
+                                AScodingViewModel.GradleDaemonMinutes = 0;
+                                AScodingViewModel.GradleDaemonSeconds = 0;
+                                AScodingViewModel.GradleDaemonMilliseconds = 0;
+
+                                AScodingViewModel.RunBuildHours = 0;
+                                AScodingViewModel.RunBuildMinutes = 0;
+                                AScodingViewModel.RunBuildSeconds = 0;
+                                AScodingViewModel.RunBuildMilliseconds = 0;
+
+                                AScodingViewModel.LoadBuildHours = 0;
+                                AScodingViewModel.LoadBuildMinutes = 0;
+                                AScodingViewModel.LoadBuildSeconds = 0;
+                                AScodingViewModel.LoadBuildMilliseconds = 0;
+
+                                AScodingViewModel.ConfigureBuildHours = 0;
+                                AScodingViewModel.ConfigureBuildMinutes = 0;
+                                AScodingViewModel.ConfigureBuildSeconds = 0;
+                                AScodingViewModel.ConfigureBuildMilliseconds = 0;
+
+                                AScodingViewModel.AllProjectsHours = 0;
+                                AScodingViewModel.AllProjectsMinutes = 0;
+                                AScodingViewModel.AllProjectsSeconds = 0;
+                                AScodingViewModel.AllProjectsMilliseconds = 0;
+
+                            }
                             break;
                         }
                     case LOG.CATEGORY.GRAPHICS:
@@ -111,7 +148,7 @@ namespace Data_Logger_1._3.Commands
                             flexiViewModel.FlexibleLogCategory = string.Empty;
                             flexiViewModel.Medium = "Song";
                             flexiViewModel.Format = "CD";
-                            flexiViewModel.BitRate = string.Empty;
+                            flexiViewModel.Bitrate = string.Empty;
                             flexiViewModel.Duration = string.Empty;
                             flexiViewModel.IsCompleted = false;
                             flexiViewModel.Source = string.Empty;
