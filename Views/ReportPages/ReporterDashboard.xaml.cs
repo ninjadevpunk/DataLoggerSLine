bf3631ced9dd50c6f9887bfcd2c8273b2952fa65
Anda Mbambo <andambambo@gmail.com>
2025-02-17 03:35:12 +0200

    Created Reporter pages for UI.

---
+++ Views/ReportPages/ReporterDashboard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Data_Logger_1._3.Views.ReportPages
+{
+    /// <summary>
+    /// Interaction logic for ReporterDashboard.xaml
+    /// </summary>
+    public partial class ReporterDashboard : Page
+    {
+        public ReporterDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
