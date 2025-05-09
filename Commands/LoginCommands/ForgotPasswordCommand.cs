44df814bc59a30566e8471818f623ba6517d7452
Anda Mbambo <andambambo@gmail.com>
2025-05-09 23:05:59 +0200

    Created ForgotPasswordCommand. Needs implementing.

---
+++ Commands/LoginCommands/ForgotPasswordCommand.cs
@@ -0,0 +1,25 @@
+﻿
+using MVVMEssentials.Commands;
+using System.Diagnostics;
+
+/// <summary>
+/// Command for forgot password page
+/// </summary>
+public class ForgotPasswordCommand : CommandBase
+{
+    public ForgotPasswordCommand()
+    {
+    }
+
+    public override void Execute(object parameter)
+    {
+        try
+        {
+            // Your implementation here
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Exception occurred near ForgotPasswordCommand.Execute(): {e.Message}");
+        }
+    }
+}
