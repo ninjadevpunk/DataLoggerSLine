fd8e69e936a651c1379560f9d28fbdde50448ff9
Anda Mbambo <andambambo@gmail.com>
2025-12-02 18:34:08 +0200

    Added missing content.
    Updated log names.

--- ViewModels/Reporter/Logs/REPORTViewModel.cs
+++ ViewModels/Reporter/Logs/REPORTViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using Data_Logger_1._3.Models;
 using Data_Logger_1._3.Services;
+using Data_Logger_1._3.ViewModels.Reporter.Updater;
 using MVVMEssentials.ViewModels;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 using System.Windows.Input;
 using static Data_Logger_1._3.Services.Cachemaster;
 
@@ -45,24 +48,56 @@ namespace Data_Logger_1._3.ViewModels.Reporter
             $"{_LOG.End.ToString("dddd, d MMMM yyyy HH:mm:ss.fff")}";
 
         /** Store the first occurence of a note with acceptable input only. **/
-        public string NotaryContent => string.Empty;
+        public string NotaryContent => _LOG.Content;
 
 
 
 
 
-        public ICommand SingleExport {  get; set; }
+        public ICommand SingleExport { get; set; }
 
-        public ICommand Edit {  get; set; }
+        public ICommand EditLogCommand { get; set; }
 
-        public ICommand View {  get; set; }
+        public ICommand ViewLogCommand { get; set; }
 
-        public ICommand Delete { get; set; }
-
-
+        public ICommand DeleteLogCommand { get; set; }
 
 
 
+
+
+
+
+        #endregion
+
+
+
+
+
+
+        #region Member Functions
+
+
+
+        public static string EF_PostItContent(ObservableCollection<EF_PostItViewModel> efPostItViewModel)
+        {
+            string pattern = @"\S";
+            Regex regex = new Regex(pattern);
+
+            foreach (EF_PostItViewModel postItViewModel in efPostItViewModel)
+            {
+                if (!string.IsNullOrWhiteSpace(postItViewModel.Display_Error) && regex.IsMatch(postItViewModel.Display_Error))
+                    return postItViewModel.Display_Error;
+                if (!string.IsNullOrWhiteSpace(postItViewModel.Display_Solution) && regex.IsMatch(postItViewModel.Display_Solution))
+                    return postItViewModel.Display_Solution;
+                if (!string.IsNullOrWhiteSpace(postItViewModel.Display_Suggestion) && regex.IsMatch(postItViewModel.Display_Suggestion))
+                    return postItViewModel.Display_Suggestion;
+                if (!string.IsNullOrWhiteSpace(postItViewModel.Display_Comment) && regex.IsMatch(postItViewModel.Display_Comment))
+                    return postItViewModel.Display_Comment;
+            }
+
+            return "No Notes";
+        }
 
 
         #endregion
