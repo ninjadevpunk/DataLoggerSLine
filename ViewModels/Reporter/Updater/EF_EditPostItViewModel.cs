cca301e1656da9c74f8ee34e86d2d31b1df6494f
Anda Mbambo <andambambo@gmail.com>
2025-12-05 14:47:48 +0200

    Fixed constructors.

--- ViewModels/Reporter/Updater/EF_EditPostItViewModel.cs
+++ ViewModels/Reporter/Updater/EF_EditPostItViewModel.cs
@@ -1,31 +1,51 @@
-﻿using Data_Logger_1._3.Commands.ReporterCommands.UpdateCommands;
+﻿using System.Windows;
+using Data_Logger_1._3.Commands.ReporterCommands.UpdateCommands;
 using Data_Logger_1._3.Models.App_Models;
 using Data_Logger_1._3.Services;
 namespace Data_Logger_1._3.ViewModels.Reporter.Updater
 {
     public class EF_EditPostItViewModel : EF_PostItViewModel
     {
-        public EF_EditPostItViewModel(int id, NavigationService navigationService, DataService dataService, ReporterUpdaterViewModel reporterUpdaterViewModel,
-            EF_PostItViewModel postItViewModel) :
+        /// <summary>
+        /// This method is for creating PostIts in the reporter updater.
+        /// </summary>
+        /// <param name="navigationService">The navigation service to take the user back to the updater</param>
+        /// <param name="dataService">The data service to populate the subjects list</param>
+        /// <param name="reporterUpdaterViewModel">The updater instance resolved from DI</param>
+        public EF_EditPostItViewModel(NavigationService navigationService, DataService dataService, ReporterUpdaterViewModel reporterUpdaterViewModel) :
             base(navigationService, dataService, reporterUpdaterViewModel)
         {
-            ID = id;
-            Subject = postItViewModel.Subject;
+            Subject = string.Empty;
             PlaceholderText = string.IsNullOrEmpty(Subject) ? "Subject" : string.Empty;
 
-            Error = postItViewModel.Error;
-            Solution = postItViewModel.Solution;
-            Suggestion = postItViewModel.Suggestion;
-            Comment = postItViewModel.Comment;
+            Subjects = new();
 
-            Display_Error = postItViewModel.Display_Error;
-            Display_Solution = postItViewModel.Display_Solution;
-            Display_Suggestion = postItViewModel.Display_Suggestion;
-            Display_Comment = postItViewModel.Display_Comment;
+            ErrorVisible = Visibility.Collapsed;
+            SolutionVisible = Visibility.Collapsed;
+            SuggestionVisible = Visibility.Collapsed;
+            CommentVisible = Visibility.Collapsed;
 
-            PostCommand = new EF_PostCommand(EF_PostCommand.ActionType.Edit, _navigationService, _reporterUpdaterViewModel, this, postItViewModel);
+            Error = "";
+            Solution = "";
+            Suggestion = "";
+            Comment = "";
+
+            Option1Check = true;
+
+            PostCommand = new EF_PostCommand(_navigationService, _reporterUpdaterViewModel, this);
         }
 
+
+
+        /// <summary>
+        /// This method is for editing PostIts in the reporter updater.
+        /// </summary>
+        /// <param name="id">The PostIt ID given by EntityFramework</param>
+        /// <param name="navigationService">The navigation service to take the user back to the updater</param>
+        /// <param name="dataService">The data service to populate the subjects list</param>
+        /// <param name="ReporterUpdaterViewModel">The updater instance resolved from DI</param>
+        /// <param name="project">The project instance needed for the subjects list retrieval</param>
+        /// <param name="postItViewModel">A PostIt viewmodel to populate fields</param>
         public EF_EditPostItViewModel(int id, NavigationService navigationService, DataService dataService, ReporterUpdaterViewModel ReporterUpdaterViewModel, ProjectClass project,
             EF_PostItViewModel postItViewModel) :
             base(navigationService, dataService, ReporterUpdaterViewModel, project)
