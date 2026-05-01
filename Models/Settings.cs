813631985b9cebb8c1c17aa02d67c82efc6e6735
Anda Mbambo <andambambo@gmail.com>
2026-05-01 21:30:40 +0200

    Created a Settings model for the application's settings.

---
+++ Models/Settings.cs
@@ -0,0 +1,48 @@
+﻿using Data_Logger_1._3.Models.App_Models;
+
+namespace Data_Logger_1._3.Models
+{
+    public enum Theme
+    {
+        Default,
+        Light,
+        Grey
+    }
+
+    public class Settings
+    {
+        public enum Theme
+        {
+            Default,
+            Light,
+            Grey
+        }
+
+        public Theme AppTheme { get; set; } = Theme.Default;
+
+        public UserSettings User { get; set; } = new UserSettings();
+
+        public void SelectedTheme(string theme)
+        {
+            switch (theme)
+            {
+                case "Light":
+                    {
+                        AppTheme = Theme.Light;
+                        break;
+                    }
+                case "Grey":
+                    {
+                        AppTheme = Theme.Grey;
+                        break;
+                    }
+                default:
+                    {
+                        AppTheme = Theme.Default;
+                        return;
+                    }
+            }
+        }
+
+    }
+}
