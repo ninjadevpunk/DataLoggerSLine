850880ffb1f32447150cfae6c8bfa47e8d27cfa0
Anda Mbambo <andambambo@gmail.com>
2026-05-01 21:32:41 +0200

    Fixed AppData location. Changed from Roaming to Local. Added try catch and made LoadImage nullable.

--- Services/BitmapService.cs
+++ Services/BitmapService.cs
@@ -12,41 +12,55 @@ namespace Data_Logger_1._3.Services
 
         public static string SaveResizedImage(string path)
         {
-            var bitmap = new BitmapImage(new Uri(path));
-
-            int size = 128;
-
-            double scale = Math.Min(
-                (double)size / bitmap.PixelWidth,
-                (double)size / bitmap.PixelHeight
-            );
-
-            var resized = new TransformedBitmap(bitmap, new ScaleTransform(scale, scale));
-
-            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Data Logger", "Profile Pics");
-            Directory.CreateDirectory(folder);
-            string newPath = Path.Combine(folder, Guid.NewGuid() + ".png");
-
-            using (var stream = new FileStream(newPath, FileMode.Create))
+            try
             {
-                var encoder = new PngBitmapEncoder();
-                encoder.Frames.Add(BitmapFrame.Create(resized));
-                encoder.Save(stream);
-            }
+                var bitmap = new BitmapImage(new Uri(path));
 
-            return newPath;
+                int size = 128;
+
+                double scale = Math.Min(
+                    (double)size / bitmap.PixelWidth,
+                    (double)size / bitmap.PixelHeight
+                );
+
+                var resized = new TransformedBitmap(bitmap, new ScaleTransform(scale, scale));
+
+                string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Data Logger", "Profile Pics");
+                Directory.CreateDirectory(folder);
+                string newPath = Path.Combine(folder, Guid.NewGuid() + ".png");
+
+                using (var stream = new FileStream(newPath, FileMode.Create))
+                {
+                    var encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(resized));
+                    encoder.Save(stream);
+                }
+
+                return newPath;
+            }
+            catch (Exception)
+            {
+                return string.Empty;
+            }
         }
 
-        public static BitmapImage LoadImage(string path)
+        public static BitmapImage? LoadImage(string path)
         {
-            var bitmap = new BitmapImage();
-            bitmap.BeginInit();
-            bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
-            bitmap.CacheOption = BitmapCacheOption.OnLoad;
-            bitmap.EndInit();
-            bitmap.Freeze();
+            try
+            {
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
 
-            return bitmap;
+                return bitmap;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
