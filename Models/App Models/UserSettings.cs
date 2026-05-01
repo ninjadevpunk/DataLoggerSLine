19a6ba8f87e470052403261ebcac980e65bbab67
Anda Mbambo <andambambo@gmail.com>
2026-05-01 21:30:19 +0200

    Created a User settings model.

---
+++ Models/App Models/UserSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Data_Logger_1._3.Models.App_Models
+{
+    public class UserSettings
+    {
+        public int Id { get; set; } = 0;
+        public string ProfilePic { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public string Email { get; set; } = "";
+        public bool IsCompanyEmployee { get; set; } = false;
+        public string? CompanyName { get; set; } = null;
+        public string? CompanyAddress { get; set; } = null;
+    }
+}
