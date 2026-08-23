783a1ad872572ff157085f3e9528744a901bcc13
Anda Mbambo <andambambo@gmail.com>
2026-08-23 14:25:59 +0200

    Changed start up object. Added Velopack dependency.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Velopack;
+
+namespace Data_Logger_1._3;
+
+public static class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+
+        var app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
