afb66115890b45ac40a4445fcb769ece72f75bbf
Anda Mbambo <andambambo@gmail.com>
2026-08-23 13:33:33 +0200

    Implemented cancel command

---
+++ Commands/CancelUpdateCommand.cs
@@ -0,0 +1,32 @@
+﻿using Data_Logger_1._3.Services;
+using MVVMEssentials.Commands;
+using System.Windows;
+
+namespace Data_Logger_1._3.Commands
+{
+    public class CancelUpdateCommand : AsyncCommandBase
+    {
+        private readonly IDataService _dataService;
+
+        public CancelUpdateCommand(IDataService dataService)
+        {
+            _dataService = dataService;
+        }
+
+
+        protected override async Task ExecuteAsync(object parameter)
+        {
+            try
+            {
+                // Close Updater Window
+                if (parameter is Window window)
+                    window.DialogResult = false;
+            }
+            catch (Exception ex)
+            {
+                await _dataService.HandleExceptionAsync(ex, "Exception occurred in CancelCommand.ExecuteAsync");
+            }
+        }
+
+    }
+}
