27fe83c41e2154189e455d7a4fd159b3723b941a
Anda Mbambo <andambambo@gmail.com>
2026-08-25 22:48:38 +0200

    Moved Velopack build to Release config

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ public static class Program
     [STAThread]
     public static void Main(string[] args)
     {
+#if RELEASE
         VelopackApp.Build().Run();
+#endif
 
         var app = new App();
         app.InitializeComponent();
