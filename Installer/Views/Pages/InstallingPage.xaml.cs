ed39831b6926bfabc0a66a9fca0baabf157201bc
Anda Mbambo <andambambo@gmail.com>
2026-08-28 01:49:53 +0200

    Scaffolded pages for install and launch step

---
+++ Installer/Views/Pages/InstallingPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Installer.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for InstallingPage.xaml
+    /// </summary>
+    public partial class InstallingPage : UserControl
+    {
+        private readonly MainWindow _mainWindow;
+
+        public InstallingPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+    }
+}
---
+++ Installer/Views/Pages/LaunchPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+
+namespace Installer.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for LaunchPage.xaml
+    /// </summary>
+    public partial class LaunchPage : UserControl
+    {
+        private readonly MainWindow _mainWindow;
+
+        public LaunchPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+    }
+}
