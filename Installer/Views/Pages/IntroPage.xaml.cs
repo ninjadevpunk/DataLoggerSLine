0cee71442c1680699e909f3696139dfa73b1f913
Anda Mbambo <andambambo@gmail.com>
2026-08-28 01:43:15 +0200

    Implemented intro page

---
+++ Installer/Views/Pages/IntroPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+
+namespace Installer.Views.Pages
+{
+    /// <summary>
+    /// Interaction logic for IntroPage.xaml
+    /// </summary>
+    public partial class IntroPage : UserControl
+    {
+        private readonly MainWindow _mainWindow;
+
+        public IntroPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            _mainWindow = mainWindow;
+        }
+
+        private void on_NEXT_Clicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _mainWindow.ShowSetupPage();
+        }
+    }
+}
