bd224f7ade3b91b7a627f627c323bd9a7a125edf
Anda Mbambo <andambambo@gmail.com>
2026-08-30 22:48:01 +0200

    Implemented launch logic. Implemented UI

--- Installer/Views/Pages/LaunchPage.xaml.cs
+++ Installer/Views/Pages/LaunchPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Controls;
 
 namespace Installer.Views.Pages
 {
@@ -14,5 +16,37 @@ namespace Installer.Views.Pages
             InitializeComponent();
             _mainWindow = mainWindow;
         }
+
+
+
+
+        private void on_FINISH_Clicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Start Data Logger if Launch checkbox is checked
+            if (checkBox_LAUNCH.IsChecked == true)
+            {
+                try
+                {
+                    string installPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Data Logger");
+                    string exePath = Path.Combine(installPath, "Data Logger.exe");
+
+                    if (File.Exists(exePath))
+                    {
+                        Process.Start(new ProcessStartInfo
+                        {
+                            FileName = exePath,
+                            UseShellExecute = true
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Failed to launch Data Logger: {ex.Message}");
+                }
+            }
+
+            _mainWindow.Close();
+        }
+
     }
 }
